565e3442a1e5f6d9be769f3198951d72d29513c4
Runze Yang <78414187+runze1223@users.noreply.github.com>
2026-03-29 10:20:38 +0800

    Update ECL.sh

--- scripts/long_term_forecast/ECL.sh
+++ scripts/long_term_forecast/ECL.sh
@@ -59,3 +59,60 @@ do
     --criterion 'MSE'\
     --itr 1 --batch_size 16 --learning_rate 0.0001 >logs/LongForecasting_new/ECL_mlow_$model_name'_96_'$pred_len.log  
 done
+
+
+seq_len=96
+model_name=CycleNet
+for pred_len in 96 192 336 720
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/ \
+    --data_path electricity.csv \
+    --model_id  ECL\
+    --model  $model_name \
+    --data custom \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 321 \
+    --dec_in 321 \
+    --c_out 321\
+    --d_model 512\
+    --cycle 168\
+    --des 'Exp' \
+    --train_epochs 60\
+    --patience 6\
+    --lradj 'TST'\
+    --criterion 'MAE'\
+    --itr 1 --batch_size 64 --learning_rate 0.0005 >logs/LongForecasting_new/ECL_mlow_$model_name'_96_'$pred_len.log  
+done
+
+
+
+seq_len=96
+model_name=NLinear
+for pred_len in 96 192 336 720
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/ \
+    --data_path electricity.csv \
+    --model_id  ECL\
+    --model  $model_name \
+    --data custom \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 321 \
+    --dec_in 321 \
+    --c_out 321\
+    --des 'Exp' \
+    --train_epochs 60\
+    --patience 8\
+    --lradj 'ST'\
+    --criterion 'MAE'\
+    --itr 1 --batch_size 16 --learning_rate 0.0001 >logs/LongForecasting_new/ECL_mlow_$model_name'_96_'$pred_len.log  
+done
