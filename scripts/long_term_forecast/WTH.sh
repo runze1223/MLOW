0a295035729f3136fafdf4a973e3173f5a0c716e
Runze Yang <78414187+runze1223@users.noreply.github.com>
2026-03-29 10:23:51 +0800

    Add long-term forecasting for CycleNet and NLinear models

--- scripts/long_term_forecast/WTH.sh
+++ scripts/long_term_forecast/WTH.sh
@@ -84,3 +84,57 @@ do
     --itr 1 --batch_size 16 --learning_rate 0.0001 >logs/LongForecasting_new/Weather_Mlow_$model_name'_96_'$pred_len.log  
 done
 
+seq_len=96
+model_name=CycleNet
+for pred_len in 96 192 336 720
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/ \
+    --data_path weather.csv\
+    --model_id  weather\
+    --model  $model_name \
+    --data custom \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 21 \
+    --dec_in 21 \
+    --c_out 21\
+    --des 'Exp' \
+    --train_epochs 60\
+    --cycle 144 \
+    --d_model 512\
+    --patience 10\
+    --lradj 'TST'\
+    --itr 1 --batch_size 128 --learning_rate 0.0001 >logs/LongForecasting_new/Weather_Mlow_$model_name'_96_'$pred_len.log  
+done
+
+
+seq_len=96
+model_name=NLinear
+for pred_len in 96 192 336 720
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/ \
+    --data_path weather.csv\
+    --model_id  weather\
+    --model  $model_name \
+    --data custom \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 21 \
+    --dec_in 21 \
+    --c_out 21\
+    --des 'Exp' \
+    --train_epochs 100\
+    --patience 10\
+    --lradj 'ST'\
+    --itr 1 --batch_size 128 --learning_rate 0.0001 >logs/LongForecasting_new/Weather_Mlow_$model_name'_96_'$pred_len.log  
+done
+
+
