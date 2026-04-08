8393e4a09db1951ac586f00aec1b927f727811a2
Runze Yang <78414187+runze1223@users.noreply.github.com>
2026-04-08 11:04:00 +0900

    Add files via upload

---
+++ A simpler version with bounded time-frequency/scripts/long_term_forecast/Traffic.sh
@@ -0,0 +1,122 @@
+export CUDA_VISIBLE_DEVICES=0
+
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/LongForecasting_new" ]; then
+    mkdir ./logs/LongForecasting_new
+fi
+
+
+seq_len=96
+model_name=iTransformer
+for pred_len in 96 192 336 720
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/ \
+    --data_path traffic.csv \
+    --model_id  Traffic\
+    --model  $model_name \
+    --data custom \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 862 \
+    --dec_in 862 \
+    --c_out 862\
+    --des 'Exp' \
+    --e_layer 5\
+    --train_epochs 150\
+    --patience 10\
+    --lradj 'TST'\
+    --std 1\
+    --itr 1 --batch_size 16 --learning_rate 0.0001 >logs/LongForecasting_new/Traffic_Mlow_$model_name'_96_'$pred_len.log  
+done
+
+seq_len=96
+model_name=PatchTST
+for pred_len in 96 192 336 720
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/ \
+    --data_path traffic.csv \
+    --model_id  Traffic\
+    --model  $model_name \
+    --data custom \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 862 \
+    --dec_in 862 \
+    --c_out 862\
+    --des 'Exp' \
+    --e_layer 5\
+    --train_epochs 150\
+    --patience 10\
+    --lradj 'TST'\
+    --std 1\
+    --criterion 'MSE'\
+    --itr 1 --batch_size 16 --learning_rate 0.0001 >logs/LongForecasting_new/Traffic_Mlow_$model_name'_96_'$pred_len.log  
+done
+
+seq_len=96
+model_name=CycleNet
+for pred_len in 96 192 336 720
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/ \
+    --data_path traffic.csv \
+    --model_id  Traffic\
+    --model  $model_name \
+    --data custom \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 862 \
+    --dec_in 862 \
+    --c_out 862\
+    --des 'Exp' \
+    --d_model 512\
+    --cycle 168\
+    --train_epochs 60\
+    --patience 6\
+    --lradj 'TST'\
+    --std 1\
+    --itr 1 --batch_size 32 --learning_rate 0.0002 >logs/LongForecasting_new/Traffic_Mlow_$model_name'_96_'$pred_len.log  
+done
+
+
+seq_len=96
+model_name=NLinear
+for pred_len in 96 192 336 720
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/ \
+    --data_path traffic.csv \
+    --model_id  Traffic\
+    --model  $model_name \
+    --data custom \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 862 \
+    --dec_in 862 \
+    --c_out 862\
+    --des 'Exp' \
+    --e_layer 5\
+    --train_epochs 60\
+    --patience 8\
+    --lradj 'ST'\
+    --std 1\
+    --itr 1 --batch_size 16 --learning_rate 0.0001 >logs/LongForecasting_new/Traffic_Mlow_$model_name'_96_'$pred_len.log  
+done
+
---
+++ A simpler version with bounded time-frequency/scripts/short_term_forecast/PEMS.sh
@@ -0,0 +1,275 @@
+export CUDA_VISIBLE_DEVICES=0
+
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/ShortForecasting_new" ]; then
+    mkdir ./logs/ShortForecasting_new
+fi
+
+
+seq_len=96
+model_name=iTransformer
+
+for pred_len in 12 24 48 96
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/PEMS/ \
+    --data_path PEMS08.npz \
+    --model_id  PEMS08\
+    --model  $model_name \
+    --data PEMS \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 170 \
+    --dec_in 170 \
+    --c_out 170\
+    --des 'Exp' \
+    --train_epochs 150\
+    --patience 20\
+    --itr 1 --batch_size 64 --learning_rate 0.0004 >logs/ShortForecasting_new/PEMS08_mlow2_$model_name'_10_336_96_'$pred_len.log  
+done
+
+
+
+seq_len=96
+model_name=iTransformer
+
+for pred_len in 12 24 48 96
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/PEMS/ \
+    --data_path PEMS03.npz \
+    --model_id  PEMS03\
+    --model  $model_name \
+    --data PEMS \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 358 \
+    --dec_in 358 \
+    --c_out 358\
+    --des 'Exp' \
+    --train_epochs 150\
+    --patience 20\
+    --itr 1 --batch_size 64 --learning_rate 0.0004 >logs/ShortForecasting_new/PEMS03_mlow2_$model_name'_10_336_96_'$pred_len.log  
+done
+
+
+
+seq_len=96
+model_name=iTransformer
+
+for pred_len in 12 24 48 96
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/PEMS/ \
+    --data_path PEMS07.npz \
+    --model_id  PEMS07\
+    --model  $model_name \
+    --data PEMS \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 883 \
+    --dec_in 883 \
+    --c_out 883\
+    --des 'Exp' \
+    --train_epochs 150\
+    --patience 20\
+    --itr 1 --batch_size 64 --learning_rate 0.0004 >logs/ShortForecasting_new/PEMS07_mlow_$model_name'_10_336_96_'$pred_len.log  
+done
+
+seq_len=96
+model_name=iTransformer
+for pred_len in 12 24 48 96
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/PEMS/ \
+    --data_path PEMS04.npz \
+    --model_id  PEMS04\
+    --model  $model_name \
+    --data PEMS \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 307 \
+    --dec_in 307 \
+    --c_out 307\
+    --des 'Exp' \
+    --train_epochs 150\
+    --patience 20\
+    --itr 1 --batch_size 64 --learning_rate 0.0004 >logs/ShortForecasting_new/PEMS04_mlow_$model_name'_10_336_96_'$pred_len.log  
+done
+
+
+
+
+
+seq_len=96
+model_name=PatchTST
+
+for pred_len in 12 24 48 96
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/PEMS/ \
+    --data_path PEMS08.npz \
+    --model_id  PEMS08\
+    --model  $model_name \
+    --data PEMS \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 170 \
+    --dec_in 170 \
+    --c_out 170\
+    --des 'Exp' \
+    --train_epochs 150\
+    --patience 20\
+    --itr 1 --batch_size 64 --learning_rate 0.0004 >logs/ShortForecasting_new/PEMS08_mlow_$model_name'_10_336_96_'$pred_len.log  
+done
+
+
+
+seq_len=96
+model_name=PatchTST
+
+for pred_len in 12 24 48 96
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/PEMS/ \
+    --data_path PEMS03.npz \
+    --model_id  PEMS03\
+    --model  $model_name \
+    --data PEMS \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 358 \
+    --dec_in 358 \
+    --c_out 358\
+    --des 'Exp' \
+    --train_epochs 150\
+    --patience 20\
+    --itr 1 --batch_size 64 --learning_rate 0.0004 >logs/ShortForecasting_new/PEMS03_mlow_$model_name'_10_336_96_'$pred_len.log  
+done
+
+
+
+seq_len=96
+model_name=PatchTST
+
+for pred_len in 12 24 48 96
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/PEMS/ \
+    --data_path PEMS07.npz \
+    --model_id  PEMS07\
+    --model  $model_name \
+    --data PEMS \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 883 \
+    --dec_in 883 \
+    --c_out 883\
+    --des 'Exp' \
+    --train_epochs 150\
+    --patience 20\
+    --itr 1 --batch_size 64 --learning_rate 0.0004 >logs/ShortForecasting_new/PEMS07_mlow_$model_name'_10_336_96_'$pred_len.log  
+done
+
+seq_len=96
+model_name=PatchTST
+
+for pred_len in 12 24 48 96
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/PEMS/ \
+    --data_path PEMS04.npz \
+    --model_id  PEMS04\
+    --model  $model_name \
+    --data PEMS \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 307 \
+    --dec_in 307 \
+    --c_out 307\
+    --des 'Exp' \
+    --train_epochs 150\
+    --patience 20\
+    --itr 1 --batch_size 64 --learning_rate 0.0004 >logs/ShortForecasting_new/PEMS04_mlow_$model_name'_10_336_96_'$pred_len.log  
+done
+
+seq_len=96
+model_name=CycleNet
+
+for pred_len in 12 24 48 96
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/PEMS/ \
+    --data_path PEMS08.npz \
+    --model_id  PEMS08\
+    --model  $model_name \
+    --data PEMS \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 170 \
+    --dec_in 170 \
+    --c_out 170\
+    --des 'Exp' \
+    --d_model 512\
+    --cycle 168\
+    --train_epochs 100\
+    --patience 10\
+    --itr 1 --batch_size 64 --learning_rate 0.0005 >logs/ShortForecasting_new/PEMS08_mlow_$model_name'_10_336_96_'$pred_len.log  
+done
+
+
+seq_len=96
+model_name=NLinear
+
+for pred_len in 12 24 48 96
+do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1\
+    --root_path ./dataset/PEMS/ \
+    --data_path PEMS08.npz \
+    --model_id  PEMS08\
+    --model  $model_name \
+    --data PEMS \
+    --features M \
+    --seq_len $seq_len\
+    --pred_len $pred_len \
+    --enc_in 170 \
+    --dec_in 170 \
+    --c_out 170\
+    --des 'Exp' \
+    --train_epochs 100\
+    --patience 10\
+    --itr 1 --batch_size 64 --learning_rate 0.0004 >logs/ShortForecasting_new/PEMS08_mlow_$model_name'_10_336_96_'$pred_len.log  
+done
+
